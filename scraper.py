4cd1eb07728a178338b5e0010f6f2b4a0b00a37c
aayushmpatra <aayushmpatra2025@gmail.com>
2026-06-26 03:00:10 -0500

    rudimentary scraper working
    parsed oscar html to get string representation of prerequisites

--- scraper.py
+++ scraper.py
@@ -1,11 +1,23 @@
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
 import requests
 
-course = requests.get('https://oscar.gatech.edu/bprod/bwckctlg.p_disp_course_detail?cat_term_in=202602&subj_code_in=AE&crse_numb_in=1601').text
+course_url = 'https://oscar.gatech.edu/bprod/bwckctlg.p_disp_course_detail?cat_term_in=202602&subj_code_in=MATH&crse_numb_in=2551'
+course = requests.get(course_url).text
 soup = BeautifulSoup(course, 'html.parser')
 
 prereq_string = ""
 
-prereq_block = soup.find('span', string='Prerequisites: ')
+prereq_block = soup.find('span', class_='fieldlabeltext', string='Prerequisites: ')
 
-print(prereq_block)
+if prereq_block is not None:
+    for item in prereq_block.next_siblings:
+        if isinstance(item, NavigableString):
+            prereq_string += item.strip()
+        elif isinstance(item, Tag):
+            if (item.string is not None):
+                prereq_string += item.string
+        prereq_string += " "
+
+
+print(prereq_string.strip())
