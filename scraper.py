8ce334361acf329e7d6e3194a12d03336b0778d3
aayushmpatra <aayushmpatra2025@gmail.com>
2026-06-27 02:20:41 -0500

    added tokenize function

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from bs4.element import NavigableString, Tag
 import requests
 
-def prereq_scraper(dept, course_code):
+def scrape_prereqs(dept, course_code):
     course_url = f'https://oscar.gatech.edu/bprod/bwckctlg.p_disp_course_detail?cat_term_in=202602&subj_code_in={dept}&crse_numb_in={course_code}'
     course = requests.get(course_url).text
     soup = BeautifulSoup(course, 'html.parser')
@@ -25,3 +25,121 @@ def prereq_scraper(dept, course_code):
     prereq_string = ' '.join(prereq_string.split())
 
     return prereq_string.strip()
+
+def tokenize(prereq_string):
+    tokens = []
+    keys = ['and', 'or', '(', ')']
+    course_codes = {
+        'ACCT',
+        'AE',
+        'AS',
+        'APPH',
+        'ASE',
+        'ARBC',
+        'ARCH',
+        'AECT',
+        'BIOS',
+        'BIOL',
+        'BMEJ',
+        'BMED',
+        'BMEM',
+        'BC',
+        'CETL',
+        'CHBE',
+        'CHEM',
+        'CHIN',
+        'CP',
+        'CEE',
+        'COE',
+        'CLL',
+        'COS',
+        'CX',
+        'CSE',
+        'CS',
+        'COOP',
+        'UCGA',
+        'EAS',
+        'ECON',
+        'ECEP',
+        'ECE',
+        'ENGL',
+        'FS',
+        'FREE',
+        'FREN',
+        'GT',
+        'GTL',
+        'GRMN',
+        'GMC',
+        'HS',
+        'HEBW',
+        'HIN',
+        'HIST',
+        'HTS',
+        'HUM',
+        'ID',
+        'ISYE',
+        'INTA',
+        'IL',
+        'INTN',
+        'IMBA',
+        'IAC',
+        'JAPN',
+        'KOR',
+        'LATN',
+        'LS',
+        'LING',
+        'LMC',
+        'MGT',
+        'MOT',
+        'MLDR',
+        'MSE',
+        'MATH',
+        'ME',
+        'MP',
+        'MSL',
+        'ML',
+        'MUSI',
+        'NS',
+        'NEUR',
+        'NRE',
+        'PERS',
+        'PHIL',
+        'PHYS',
+        'POL',
+        'PTFE',
+        'PORT',
+        'PSYC',
+        'PUBJ',
+        'PUBP',
+        'RUSS',
+        'SCI',
+        'SLS',
+        'SS',
+        'SOC',
+        'SPAN',
+        'SWAH',
+        'VIP',
+        'WOLOF'
+
+    }
+    i = 0
+
+    token_list = prereq_string.split()
+    
+
+    while i < len(token_list):
+        token = token_list[i]
+        if token in keys:
+            tokens.append(token)
+        elif token in course_codes and token_list[i+1][1].isnumeric():
+            tokens.append(f'{token} {token_list[i+1]}')
+        i += 1
+    
+    cleaned_tokens = []
+    for token in tokens:
+        if (token == 'or' or token == 'and') and cleaned_tokens and cleaned_tokens[-1] == token:
+            continue
+        else:
+            cleaned_tokens.append(token)
+
+    return cleaned_tokens
