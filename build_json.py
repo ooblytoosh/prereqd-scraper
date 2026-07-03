7b801d68ded9d3b5b96f4d6811315904d186293f
aayushmpatra <aayushmpatra2025@gmail.com>
2026-07-03 17:20:16 -0500

    added transfer credit and labs to courses.json

--- build_json.py
+++ build_json.py
@@ -12,13 +12,7 @@ with requests.Session() as session:
         dept = course_components[0]
         code = course_components[1]
         count += 1
-
         current_time = datetime.now().strftime("%H:%M:%S")
-
-        if not code.isdigit():
-            print(f"[{current_time}] ({count}/2892) Skipped non-numeric code {course}")
-            continue
-
         start_time = time.perf_counter()
 
         course_name, prereq_string = scrape_prereqs(dept, code, session)
@@ -33,7 +27,7 @@ with requests.Session() as session:
 
         duration = time.perf_counter() - start_time
         current_time = datetime.now().strftime("%H:%M:%S")
-        print(f"[{current_time}] ({count}/2892) Processed {course} in {duration:.2f}s")
+        print(f"[{current_time}] ({count}/2855) Processed {course} in {duration:.2f}s")
 
 with open('courses.json', 'w') as f:
     json.dump(courses, f, indent=2)
